ffdb5db7414e3ab718e5bab0f87ceee7d22fe547
tengueax <tengueax@tutamail.com>
2024-08-20 06:12:17 -0700

    Initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,34 @@
+if (Get-Module -ListAvailable -Name PSReadLine) {
+	Set-PSReadLineOption -PredictionSource History
+} else {
+	Import-Module -Name PSReadLine -Force -Prefix PS
+	Set-PSReadLineOption -PredictionSource History
+}
+
+$env:POSH_GIT_ENABLED = $true
+
+oh-my-posh init pwsh --config "https://raw.githubusercontent.com/tengueax/powershell/main/theme.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\0_custom.omp.json" | Invoke-Expression
+
+Set-Alias -Name ll -Value ls
+
+Function gig {
+  param(
+    [Parameter(Mandatory=$true)]
+    [string[]]$list
+  )
+  $params = ($list | ForEach-Object { [uri]::EscapeDataString($_) }) -join ","
+  if($list[0] -eq "list") {
+    Invoke-WebRequest -Uri "https://www.toptal.com/developers/gitignore/api/list" | select -ExpandProperty content
+  } else {
+    Invoke-WebRequest -Uri "https://www.toptal.com/developers/gitignore/api/$params" | select -ExpandProperty content | Out-File -FilePath $(Join-Path -path $pwd -ChildPath ".gitignore") -Encoding ascii
+  }
+}
+
+filter Filter-Object ([string]$pattern) {
+    Out-String -InputObject $_ -Stream | Select-String -Pattern "$pattern"
+}
+
+Set-Alias -Name grep -Value Filter-Object
+
+Start-Service ssh-agent
